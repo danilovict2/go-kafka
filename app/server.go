99db49a1ecaff2f416802664b1b7873a9b53c792
danilovict2 <danilosindric@gmail.com>
2024-11-28 21:12:56 +0100

    send a response with a correlation ID

--- app/server.go
+++ app/server.go
@@ -1,29 +1,38 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"io"
 	"net"
 	"os"
 )
 
-// Ensures gofmt doesn't remove the "net" and "os" imports in stage 1 (feel free to remove this!)
-var _ = net.Listen
-var _ = os.Exit
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
-
-	// Uncomment this block to pass the first stage
-	//
 	l, err := net.Listen("tcp", "0.0.0.0:9092")
 	if err != nil {
 		fmt.Println("Failed to bind to port 9092")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
+	}
+
+	for {
+		buf := make([]byte, 1024)
+		_, err = conn.Read(buf)
+		if errors.Is(err, io.EOF) {
+			fmt.Println("Client closed connections at:", conn.RemoteAddr())
+			break
+		} else if err != nil {
+			fmt.Println(err)
+			break
+		}
+
+		conn.Write([]byte{0, 0, 0, 0, 0, 0, 0, 7})
 	}
 }
